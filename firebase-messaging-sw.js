669178e06f826cb021705438b6cb961ea7ae5300
GMW1:9 <gerald.m.wlaschin@gmail.com>
2026-09-24 17:47:33 -0400

    Implement Firebase messaging service in SW
    Added Firebase configuration and background message handling.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -22,3 +22,31 @@ messaging.onBackgroundMessage((payload) => {
     tag: "cpw-alert"
   });
 });
+
+importScripts("https://www.gstatic.com/firebasejs/12.17.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.17.1/firebase-messaging-compat.js");
+
+f// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwnxuI_EbCB2vTT2CzOhOwumuoqdZ5huM",
+  authDomain: "gmw-1-9.firebaseapp.com",
+  databaseURL: "https://gmw-1-9-default-rtdb.firebaseio.com",
+  projectId: "gmw-1-9",
+  storageBucket: "gmw-1-9.firebasestorage.app",
+  messagingSenderId: "994319050351",
+  appId: "1:994319050351:web:fc8b07dcd89bdc8b034f9a",
+  measurementId: "G-ZJFD1GVXS6"
+};
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title = payload?.notification?.title || "Catholic PrayerWeb";
+  const body = payload?.notification?.body || "You have a new update.";
+
+  self.registration.showNotification(title, {
+    body,
+    icon: "/Catholic-PrayerWeb-by-GMW1-9/cpw.png",
+    tag: "cpw-alert"
+  });
+});
